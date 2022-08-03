e00118ebd9683ab0b5bf10e5841e4e255fe21562
sl798970799 <798970799@qq.com>
2022-08-04 00:04:28 +0800

    feat:1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,5 @@
 module.exports = {
-  extends: [
-    'eslint-config-ali/typescript/react',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-  ],
+  extends: ['eslint-config-ali/typescript/react', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
   rules: {
     '@typescript-eslint/no-unused-vars': 1,
     'no-console': 0,
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,57 +1,28 @@
-import React from 'react';
-import { EllipsisOutlined } from '@ant-design/icons';
-import { PageContainer, ProCard } from '@ant-design/pro-components';
-import { Button, Dropdown, Menu } from 'antd';
+import React, { useRef } from 'react';
+import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
+import { ProTable, PageContainer, TableDropdown, ActionType, ProColumns } from '@ant-design/pro-components';
+import { Button, Dropdown, Menu, Space, Tag } from 'antd';
 
-export default () => (
-  <div
-    style={{
-      background: '#F5F7FA',
-    }}
-  >
+export default () => {
+  const actionRef = useRef<ActionType>();
+  return (
     <PageContainer
       ghost
       header={{
         title: document.title,
         breadcrumb: {},
-        extra: [
-          <Button key="1">次要按钮</Button>,
-          <Button key="2">次要按钮</Button>,
-          <Button key="3" type="primary">
-            主要按钮
-          </Button>,
-        ],
       }}
-      tabList={[
-        {
-          tab: '基本信息',
-          key: 'base',
-          closable: false,
-        },
-        {
-          tab: '详细信息',
-          key: 'info',
-        },
-      ]}
-      tabProps={{
-        type: 'editable-card',
-        hideAdd: true,
-        onEdit: (e, action) => console.log(e, action),
-      }}
-      footer={[
-        <Button key="3">重置</Button>,
-        <Button key="2" type="primary">
-          提交
-        </Button>,
-      ]}
     >
-      <ProCard direction="column" ghost gutter={[0, 16]}>
-        <ProCard style={{ height: 200 }} />
-        <ProCard gutter={16} ghost style={{ height: 200 }}>
-          <ProCard colSpan={16} />
-          <ProCard colSpan={8} />
-        </ProCard>
-      </ProCard>
+      <ProTable
+        search={false}
+        actionRef={actionRef}
+        cardBordered
+        toolBarRender={() => [
+          <Button key="button" icon={<PlusOutlined />} type="primary">
+            新建
+          </Button>,
+        ]}
+      />
     </PageContainer>
-  </div>
-);
+  );
+};
